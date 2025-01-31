eff1510d09c9046012a76b7ce77741bd39b21d78
Anxo55 <anxo.campos260204@gmail.com>
2025-01-31 08:55:42 +0100

    migrate-render

--- src/app.ts
+++ src/app.ts
@@ -25,7 +25,7 @@ app.use(limiter)
 //TODO: limitar cors
 //TODO: Cambiar URL cuando deployemos
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: '*',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true
 }
